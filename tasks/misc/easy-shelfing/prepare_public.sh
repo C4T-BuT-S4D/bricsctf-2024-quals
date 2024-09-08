a505206eb79bd9c1a17a2f18fde58a6b950850df
Georgy Kiguradze <gt.kiguradze@gmail.com>
2024-09-08 05:19:38 +0300

    [shelfing] fix public archive

--- tasks/misc/easy-shelfing/prepare_public.sh
+++ tasks/misc/easy-shelfing/prepare_public.sh
@@ -3,14 +3,14 @@
 set -ex
 
 rm -rf shelfing public/shelfing.tar.gz
-mkdir -p shelfing/deploy
+mkdir -p shelfing/service
 
-cp deploy/docker-compose.yml shelfing/deploy/
-cp deploy/service/Dockerfile shelfing/deploy/
-cp deploy/service/entrypoint.sh shelfing/deploy/
-cp deploy/service/server.py shelfing/deploy/
+cp deploy/docker-compose.yml shelfing/
+cp deploy/service/Dockerfile shelfing/service/
+cp deploy/service/entrypoint.sh shelfing/service/
+cp deploy/service/server.py shelfing/service/
 
-echo "flag{example}" > shelfing/deploy/flag.txt
+echo "flag{example}" > shelfing/service/flag.txt
 
 tar -cvf shelfing.tar ./shelfing/* && gzip -9 shelfing.tar
 mv shelfing.tar.gz public/
